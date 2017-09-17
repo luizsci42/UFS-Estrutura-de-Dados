c9227d63fbd2261fe22d036f243bf2e35e53a8a1
Aleister-Nevice <luizsci42@gmail.com>
2017-09-17 13:36:04 -0300

    Teste na função heap. Correção em breve

--- heap_sort.c
+++ heap_sort.c
@@ -2,10 +2,20 @@
 /* Transforma a array passada em um heaper.
  * Para isso, v[i/2] >= v[i] para todo i.
  */
+
+void imprimir(int array[], int length){
+	int i;
+
+	printf("heaṕ: \n");
+	for(i = 0; i < length; i++){
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+}
 void heaper(int array[], int length) {
 	int i, aux;
 
-	for (i = 1; i <= length; i++) {
+	for (i = 1; i < length; ++i) {
 		while (array[i/2] < array[i]) {
 			aux = array[i/2];
 			array[i/2] = array[i];
@@ -13,17 +23,15 @@ void heaper(int array[], int length) {
 		}
 	}
 
-	printf("heap: \n");
-	for (i = 1; i <= length; i++) {
-		printf("%d ", array[i]);
-	}
-	printf("\n");
+	imprimir(array, length);
 }
 
 int main() {
 	int valores[10] = { 8, 4, 2, 6, 3, 1, 9, 7, 5, 0 };
 	int tamanho = sizeof(valores) / sizeof(int);
 
+	heaper(valores, tamanho);
+	heaper(valores, tamanho);
 	heaper(valores, tamanho);
 
 	return 0;
