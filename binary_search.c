8b588184e1274133e1430767dab2d05de44f10e8
Luiz Felipe Souza <luizsci42@gmail.com>
2017-09-26 22:46:41 -0300

    Busca binária recursiva

--- binary_search.c
+++ binary_search.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 #include "selection_sort.h"
- 
+// #include "heap_sort.h"
+
 /* Faz uso de uma lista ordenada para procurar um valor.
  * Para isso, divide a lista no meio e verifica se o número
  * procurado é maior ou menor que o número do meio. Se for
- * menor, dividiremos a array da direita no meio e repetimos o
+ * menor, dividiremos a array ao meio e repetimos o
  * processo até encontrar o número procurado.*/
-void binary_search(int numeros[]) {
+int buscaBinaria(int x, int len, int vetor[]) {
+	return binary_search(x, 0, len, vetor);
+}
 
-	int *vetor = &numeros;
-	vetor = selection_sort(*vetor);
+int binary_search(int x, int left, int len, int v[]) {
+
+	int mid, right = len;
+
+	if(left == right) {
+		return left;
+	}
+	else {
+		mid = (left + right)/2;
+		if(x > v[mid]) {
+		return binary_search(x, mid+1, right, v);
+	}
+        else {
+            return binary_search(x, left, mid, v);
+        }
+	}
 }
