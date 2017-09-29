6d1197d12693ccc3f4a6451a49d373e98e5c5691
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2017-09-28 21:14:46 -0300

    Update Untitled1.c

--- Untitled1.c
+++ Untitled1.c
@@ -12,19 +12,21 @@ void insere_ord_Array(struct Valor vl,struct Array *a);
 void retira_Array(Apontador p, struct Array *array, struct Valor *valor);
 void imprime_Array(struct Array lista);
 
-
 struct Valor{
   int valor;
 };
+
 struct Array{
   struct Valor item[max];
   Apontador primeiro;
   Apontador ultimo;
 };
+
 void iniciar_Array(struct Array *array){
   array->primeiro=inicio;
   array->ultimo=array->primeiro;
 }
+
 void insere_ord_Array(struct Valor vl,struct Array *a){
     int j;
     int k=a->ultimo;
@@ -39,17 +41,26 @@ void insere_ord_Array(struct Valor vl,struct Array *a){
         a->ultimo++;
     }
 }
+
 void retira_Array(Apontador p, struct Array *array, struct Valor *item){
     int i;
     if(p<array->primeiro -1||p>=array->ultimo -1){
         printf("Posicao nao existe\n");
-    }else{
-        array->ultimo--;
-        for(i=p; i<array->ultimo;i++){
-            array->item[i] = array->item[i+1];
+    }
+    else{
+        for(i=0; i<array->ultimo;i++){
+            if (array->item[i].valor==item[0].valor){
+                int j;
+                for(j=i; j<array->ultimo;j++){
+                    array->item[j] = array->item[j+1];
+                }
+            array->ultimo--;
+            break;
+            }
         }
     }
 }
+
 void imprime_Array(struct Array lista){
     int i;
     for(i=lista.primeiro-1;i<lista.ultimo-1;i++){
@@ -60,20 +71,25 @@ void imprime_Array(struct Array lista){
 
 int main(){
     struct Valor item;
+    struct Valor item2;
     struct Array array;
-    iniciar_Array(&array);
     int i;
+  
+    iniciar_Array(&array);
     for (i=50; i>0; i--) {
-        //struct Valor item;
         item.valor=i;
         insere_ord_Array(item,&array);
     }
+  
     imprime_Array(array);
     printf("______________________\n");
-    retira_Array(5,&array,&item);
+
+    item2.valor=30;
+    retira_Array(1,&array,&item2);
+    item2.valor=40;
+    retira_Array(1,&array,&item2);
+  
     imprime_Array(array);
-
   return 0;
-
 }
 
